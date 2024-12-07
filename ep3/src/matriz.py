0597966f51b93c1ae3c6448cc194ae6bd9a9ac32
Júlia Calixto <juliacalixtorosaa@gmail.com>
2024-12-07 20:53:25 -0300

    documentação matriz

--- ep3/src/matriz.py
+++ ep3/src/matriz.py
@@ -1,7 +1,22 @@
 class Matriz:
+    """
+    Classe que contém métodos estáticos para manipulação de matrizes.
+
+    Esta classe cria matrizes vazias e copia uma matriz existente.
+    """
 
     @staticmethod
     def devolve_matriz_vazia(num_linhas,num_colunas):
+        """
+        Cria e retorna uma matriz vazia com o número especificado de linhas e colunas.
+
+        Cada célula da matriz é inicializada com o valor `" "`.
+
+        @param num_linhas: Número de linhas da matriz.
+        @param num_colunas: Número de colunas da matriz.
+
+        @return: Uma matriz de tamanho `num_linhas` por `num_colunas`, preenchida com espaços vazios.
+        """
 
         matriz_inicial = []  
 
@@ -15,7 +30,15 @@ class Matriz:
     
     @staticmethod
     def copia_matriz(matriz_ref):
+        """
+        Cria uma cópia de uma matriz existente.
 
+        Este método copia todos os elementos de `matriz_ref` para uma nova matriz, preservando seus valores.
+
+        @param matriz_ref: A matriz a ser copiada.
+
+        @return: Uma nova matriz contendo os mesmos elementos de `matriz_ref`.
+        """
         matriz_copia = []  
 
         for i in range(len(matriz_ref)):  
