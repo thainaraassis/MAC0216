31ac11c8a997a565336fe276300bd1f74c0d8c37
Júlia Calixto <juliacalixtorosaa@gmail.com>
2024-12-07 20:57:17 -0300

    documentação tela

--- ep3/src/tela.py
+++ ep3/src/tela.py
@@ -1,12 +1,32 @@
 import os
 
 class Tela:
+    """
+    Classe responsável por exibir o estado do jogo no terminal.
+
+    A classe cuida da renderização da matriz do jogo, exibindo as peças no terminal e os comandos
+    do jogo, além de fornecer mensagens relacionadas ao andamento da partida, como pontuação e
+    mensagens de fim de jogo.
+    """
 
     def __init__(self, matriz_partida):
+        """
+        Inicializa a classe Tela.
+
+        @param matriz_partida: Matriz que representa o estado atual do jogo, 
+        contendo as peças e o layout do tabuleiro.
+        """
+
         self.matriz_tela = matriz_partida
 
     def exibe_matriz(self):
-        """exibe a matriz do jogo no terminal com as grades"""
+        """
+        Exibe a matriz do jogo no terminal, com as bordas e as peças, representando o estado
+        atual da partida.
+
+        A função limpa a tela e, em seguida, exibe a matriz com uma borda ao redor dela.
+        """
+
         self.limpa_tela()
 
         print("+" + "-" * (len (self.matriz_tela[0])) + "+")  
@@ -21,7 +41,13 @@ class Tela:
         print("+" + "-" * (len (self.matriz_tela[0])) + "+")  
 
     def exibe_comandos(self, pontuacao):
-        """exibe as instruções do jogo """
+        """
+        Exibe as instruções e os controles do jogo no terminal.
+
+        Esta função exibe as teclas de comando disponíveis e a pontuação atual do jogador.
+
+        @param pontuacao: A pontuação atual do jogador, que será exibida ao lado das instruções.
+        """
 
         print("\nPontuação:", pontuacao)
         print("Teclas do jogo:")
@@ -31,6 +57,13 @@ class Tela:
 
 
     def exibe_comandos_game_over(self, pontuacao):
+        """
+        Exibe uma mensagem de fim de jogo, incluindo a pontuação final.
+
+        Esta função é chamada quando a partida chega ao fim, e mostra ao jogador sua pontuação final.
+
+        @param pontuacao: A pontuação final do jogador, que será exibida na tela.
+        """
         print("Fim da partida!")
         print("Pontuação final:", pontuacao)
         print("\n*** Jogo Textris - um tetris em modo texto ***")
@@ -40,5 +73,7 @@ class Tela:
         print("\n*** Jogo Textris - um tetris em modo texto ***")
 
     def limpa_tela(self):
-        """limpa o terminal para atualizar a visualização do jogo """
+        """
+        Limpa a tela do terminal para atualizar a visualização do jogo.
+        """
         os.system('cls || clear')
