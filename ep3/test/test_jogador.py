f16cbf1fc8e91c78a30678cd9bc960767ac47164
thainaraassis <thainaraassisgoulart@gmail.com>
2024-12-08 17:12:08 -0300

    ✅ update test

--- ep3/test/test_jogador.py
+++ ep3/test/test_jogador.py
@@ -1,25 +1,26 @@
 import pytest
 from jogador import Jogador
 
-def test_criar_jogador():
-    
-    jogador = Jogador("Kelly")
-    assert jogador.nome == "Kelly"
-    assert jogador.pontuacao == 0
+class Test_Jogador():
+    def test_criar_jogador(self):
+        
+        jogador = Jogador("Kelly")
+        assert jogador.nome == "Kelly"
+        assert jogador.pontuacao == 0
 
-    jogador = Jogador("Henrique", 18)
-    assert jogador.nome == "Henrique"
-    assert jogador.pontuacao == 18
+        jogador = Jogador("Henrique", 18)
+        assert jogador.nome == "Henrique"
+        assert jogador.pontuacao == 18
 
-def test_atualizar_pontuacao():
-    
-    jogador = Jogador("Livia")
-    jogador.atualizar_pontuacao(7)
-    assert jogador.pontuacao == 7  
+    def test_atualizar_pontuacao(self):
+        
+        jogador = Jogador("Livia")
+        jogador.atualizar_pontuacao(7)
+        assert jogador.pontuacao == 7  
 
-    jogador = Jogador("Julia", 11)
-    jogador.atualizar_pontuacao(3)
-    assert jogador.pontuacao == 14
+        jogador = Jogador("Julia", 11)
+        jogador.atualizar_pontuacao(3)
+        assert jogador.pontuacao == 14
 
     
 
