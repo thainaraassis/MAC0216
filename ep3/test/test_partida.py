6a503502213a984728cd8554c65d2a3429af6bf3
thainaraassis <thainaraassisgoulart@gmail.com>
2024-12-07 23:25:44 -0300

    🚧  falta um teste

--- ep3/test/test_partida.py
+++ ep3/test/test_partida.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from partida import Partida
 
 
@@ -6,7 +7,6 @@ from partida import Partida
 def partida_teste():
     partida = Partida("Teste", 10, 10)
     partida.peça_atual.matriz_peça = partida.peça_atual.gera_peça("T")
-    # desenha peças já na matriz?
     return partida
 
 
@@ -133,4 +133,180 @@ class Test_Partida():
         ]
 
         assert partida_teste.matriz_jogo == matriz_jogo_esperada
+
+    def test_atualiza_pontuaçao(self, partida_teste):
+
+        matriz_esperada = [
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "] 
+        ]
+        
+        ## teste para pontuação = 1 ##
+        partida_teste.matriz_jogo[9] = ["+"] * 10  
+        partida_teste.atualiza_pontuaçao()
+        assert partida_teste.matriz_jogo == matriz_esperada
+        assert partida_teste.pontuacao == 1
+
+        ## teste para pontuação = 3 ##
+        # reinicia o estado da partida
+        partida_teste.matriz_jogo = [
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "] 
+        ]
+
+        partida_teste.matriz_jogo[8] = ["+"] * 10
+        partida_teste.matriz_jogo[9] = ["+"] * 10
+        partida_teste.atualiza_pontuaçao()
+        assert partida_teste.matriz_jogo == matriz_esperada
+        assert partida_teste.pontuacao == 4 # 1 ponto anterior + 3
+
+        ## teste para pontuação = 5 ##
+        partida_teste.matriz_jogo = [
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "] 
+        ]
+
+        partida_teste.matriz_jogo[7] = ["+"] * 10
+        partida_teste.matriz_jogo[8] = ["+"] * 10
+        partida_teste.matriz_jogo[9] = ["+"] * 10
+        partida_teste.atualiza_pontuaçao()
+        assert partida_teste.matriz_jogo == matriz_esperada
+        assert partida_teste.pontuacao == 9 # 4 pontos anterior + 5
+
+        ## teste para pontuação = 7 ##
+        partida_teste.matriz_jogo = [
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [ " ", " ", " ", " ", " ", " ", " ", " ", " ", " "] 
+        ]
+
+        partida_teste.matriz_jogo[6] = ["+"] * 10
+        partida_teste.matriz_jogo[7] = ["+"] * 10
+        partida_teste.matriz_jogo[8] = ["+"] * 10
+        partida_teste.matriz_jogo[9] = ["+"] * 10
+        partida_teste.atualiza_pontuaçao()
+        assert partida_teste.matriz_jogo == matriz_esperada
+        assert partida_teste.pontuacao == 16 # 9 pontos anterior + 7 
+
+    def test_peça_move_na_horizontal(self, partida_teste):
+
+        partida_teste.peça_atual.pos_y = 5  # seta a posição inicial na coluna 5
+        partida_teste.movimento_horizontal("direita") # move para direita
+        assert partida_teste.peça_atual.pos_y == 6
+
+        partida_teste.movimento_horizontal("esquerda") # move para esquerda
+        assert partida_teste.peça_atual.pos_y == 5
+
+    def test_peça_nao_move_na_horizontal(self, partida_teste):
+
+        partida_teste.peça_atual.pos_y = 9 # última coluna disponivel
+        partida_teste.movimento_horizontal("direita") # tentamos mover
+
+        assert partida_teste.peça_atual.pos_y == 9
+    
+    def test_peça_rotaciona(self, partida_teste):
+
+        partida_teste.peça_atual.pos_x = 5
+        partida_teste.peça_atual.pos_y = 5
+        partida_teste.peça_atual.matriz_peça = partida_teste.peça_atual.gera_peça("T")
+        partida_teste.desenha_peça_na_matriz()
+
+        partida_teste.movimento_rotaciona("direita")
+        matriz_esperada_rotacao_direita = [
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", "+", " ", " ", " "],
+            [" ", " ", " ", " ", " ", "+", "+", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", "+", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ]
+        assert partida_teste.matriz_jogo == matriz_esperada_rotacao_direita
+
+        partida_teste.movimento_rotaciona("esquerda")
+        matriz_esperada_rotacao_esquerda = [
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", "+", "+", "+", " ", " "],
+            [" ", " ", " ", " ", " ", " ", "+", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ]
+        assert partida_teste.matriz_jogo == matriz_esperada_rotacao_esquerda
+        
+    # def test_peça_não_rotaciona(self, partida_teste):
+        
+    #     # desenhamos ela no topo, sem chance de rotacionar
+    #     partida_teste.peça_atual.pos_x = 0 
+    #     partida_teste.peça_atual.pos_y = 0
+    #     partida_teste.peça_atual.matriz_peça = partida_teste.peça_atual.gera_peça("T")
+    #     partida_teste.desenha_peça_na_matriz()
+
+    #     matriz_antes_rotacao = [
+    #         ["+", "+", "+", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", "+", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #     ]   
+        
+
+    #     partida_teste.movimento_rotaciona("direita")
+    #     assert partida_teste.matriz_jogo == matriz_antes_rotacao # não mudou 
+
+    def test_grava_sai_partida(self, partida_teste):
+        
+        partida_teste.grava_sai_partida()
+
+        arquivos_criados = [
+            arquivo for arquivo in os.listdir(".") if arquivo.endswith(".pkl")
+        ]
+        assert len(arquivos_criados) == 1
+
+        # remove o arquivo gerado após o teste
+        for arquivo in arquivos_criados:
+            os.remove(os.path.join(arquivo))
 
