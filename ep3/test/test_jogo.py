d9ab4fbc5828086626a4f5fe8b7118b47d99dbaf
thainaraassis <thainaraassisgoulart@gmail.com>
2024-12-08 17:13:34 -0300

    ✅  update test jogo

--- ep3/test/test_jogo.py
+++ ep3/test/test_jogo.py
@@ -5,58 +5,58 @@ from jogo import Jogo
 
 @pytest.fixture
 def jogo():
-
     # fixture para criar uma instância de Jogo
     return Jogo()
 
-def test_carregar_partida(capsys, jogo):
+class Test_Jogo():
+    def test_carregar_partida(self, capsys, jogo):
 
-    ## testando para quando não há arquivos ##
-    jogo.arquivos_gravados = []
+        ## testando para quando não há arquivos ##
+        jogo.arquivos_gravados = []
 
-    jogo.carregar_partida()
-    captured = capsys.readouterr()
-
-    assert "Não há partidas salvas." in captured.out
-
-    ## testando quando há arquivos e o arquivo requerido existe ##
-    jogo.arquivos_gravados = ["thai_2024_12_07.pkl"]
-    
-    with patch("builtins.input", return_value="thai_2024_12_07.pkl"):
         jogo.carregar_partida()
         captured = capsys.readouterr()
 
-        assert "Partidas salvas:" in captured.out
-        assert "thai_2024_12_07.pkl" in captured.out
+        assert "Não há partidas salvas." in captured.out
 
-    ## testando quando há arquivos e o arquivo requerido NÃO existe ##
+        ## testando quando há arquivos e o arquivo requerido existe ##
+        jogo.arquivos_gravados = ["thai_2024_12_07.pkl"]
+        
+        with patch("builtins.input", return_value="thai_2024_12_07.pkl"):
+            jogo.carregar_partida()
+            captured = capsys.readouterr()
 
-    jogo.arquivos_gravados = ["thai_2024_12_07.pkl"]
-    
-    with patch("builtins.input", return_value="ju_2024_12_06.pkl"):
-        jogo.carregar_partida()
-        captured = capsys.readouterr()
+            assert "Partidas salvas:" in captured.out
+            assert "thai_2024_12_07.pkl" in captured.out
 
-        assert "Partidas salvas:" in captured.out
-        assert "thai_2024_12_07.pkl" in captured.out
-        assert "Essa partida não existe." in captured.out
-       
+        ## testando quando há arquivos e o arquivo requerido NÃO existe ##
+
+        jogo.arquivos_gravados = ["thai_2024_12_07.pkl"]
+        
+        with patch("builtins.input", return_value="ju_2024_12_06.pkl"):
+            jogo.carregar_partida()
+            captured = capsys.readouterr()
+
+            assert "Partidas salvas:" in captured.out
+            assert "thai_2024_12_07.pkl" in captured.out
+            assert "Essa partida não existe." in captured.out
+        
 
 
-def test_sair_jogo(jogo):
+    def test_sair_jogo(self, jogo):
 
-    jogo.arquivos_gravados = ["thai_2024_12_07.pkl", "ju_2024_12_06.pkl"]
+        jogo.arquivos_gravados = ["thai_2024_12_07.pkl", "ju_2024_12_06.pkl"]
 
-    # cria os arquivos para simulaçao
-    for arquivo in jogo.arquivos_gravados:
-        with open(arquivo, "w") as f:
-            f.write("simulação")
+        # cria os arquivos para simulaçao
+        for arquivo in jogo.arquivos_gravados:
+            with open(arquivo, "w") as f:
+                f.write("simulação")
 
-    jogo.sair_jogo()
+        jogo.sair_jogo()
 
-    # verifica se os arquivos foram removidos
-    for arquivo in jogo.arquivos_gravados:
-        assert not os.path.exists(arquivo)  
+        # verifica se os arquivos foram removidos
+        for arquivo in jogo.arquivos_gravados:
+            assert not os.path.exists(arquivo)  
 
     
 
